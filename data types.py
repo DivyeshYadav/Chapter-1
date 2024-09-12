6060edddaa99991a37ce0ac5a01e909e53167ecd
DivyeshYadav <prabhakaryadav621@gmail.com>
2024-09-12 18:37:34 +0545

    DATA TYPES

---
+++ data types.py
@@ -0,0 +1,12 @@
+name = "Divyesh Yadav"
+age = 9
+height = 4.6
+is_student = True
+
+print(name,"is",type(name),"type variable")
+print(age,"is",type(age),"type variable")
+print(height,"is",type(height),"type variable")
+print(is_student,"is",type(is_student),"type variable")
+
+height = int(height)
+print(type(height))
